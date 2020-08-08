55980019210091ebbf0b79a294dacb75b18e62e4
Carlos <carlos.bergamasco@gmail.com>
2020-08-08 17:03:43 -0300

    Add members of Category class and Create Product class

--- src/main/java/com/berga/orderproducts/model/Category.java
+++ src/main/java/com/berga/orderproducts/model/Category.java
@@ -1,5 +1,50 @@
 package com.berga.orderproducts.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Category {
 
+	private Integer id;
+	private String name;
+	
+	private Set<Product> products;
+	
+	public Category() {
+	}
+	
+	public Category(String name) {
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue
+	@Column(name="CATEGORY_ID")
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@OneToMany(mappedBy="category", cascade = CascadeType.ALL)
+	public Set<Product> getProducts() {
+		return products;
+	}
 }
---
+++ src/main/java/com/berga/orderproducts/model/Product.java
@@ -0,0 +1,71 @@
+package com.berga.orderproducts.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Product {
+
+	private Integer id;
+	private String name;
+	private String description;
+	private BigDecimal price;
+	private Category category;
+	
+	public Product() {
+	}
+	
+	public Product(String name, String description, BigDecimal price, Category category) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.category = category;
+	}
+
+	@Id
+	@GeneratedValue
+	@Column(name="PRODUCT_ID")
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public BigDecimal getPrice() {
+		return price;
+	}
+	
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "CATEGORY_ID")
+	public Category getCategory() {
+		return category;
+	}
+}
