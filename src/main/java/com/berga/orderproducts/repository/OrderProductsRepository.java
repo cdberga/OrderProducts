902d472d775c9d6d8b5eda5dbde5c182f260dec0
Carlos <carlos.bergamasco@gmail.com>
2020-08-09 10:53:12 -0300

    Add session and query categories

--- src/main/java/com/berga/orderproducts/repository/OrderProductsRepository.java
+++ src/main/java/com/berga/orderproducts/repository/OrderProductsRepository.java
@@ -1,8 +1,26 @@
 package com.berga.orderproducts.repository;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+
+import com.berga.orderproducts.model.Category;
+import com.berga.orderproducts.util.HibernateUtil;
+
 public class OrderProductsRepository {
 
+	HibernateUtil util;
+
 	public void findAllCategories() {
+		Session session = util.getNewSession();
 		String hql = "from Category";
+		Query query = session.createQuery(hql);
+		List<Category> categories = query.getResultList();
+		
+		for(Category category : categories) {
+			System.out.println(category.getName());
+		}
 	}
 }
--- src/main/java/com/berga/orderproducts/util/HibernateUtil.java
+++ src/main/java/com/berga/orderproducts/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.berga.orderproducts.util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -8,8 +9,9 @@ import org.hibernate.service.ServiceRegistry;
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
+	private Session session;
 	
-	public static SessionFactory getSessionFactory() {
+	private static SessionFactory getSessionFactory() {
 		if(sessionFactory == null) {
 			// loads configurations and mappings
 			Configuration configuration = new Configuration().configure();
@@ -22,5 +24,14 @@ public class HibernateUtil {
 		}
 
 		return sessionFactory;
+	}
+	
+	public Session getNewSession() {
+		session = getSessionFactory().openSession();
+		return session;
+	}
+	
+	public void closeSession() {
+		session.close();
 	}
 }
