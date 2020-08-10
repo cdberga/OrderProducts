bf868c933e178ef069f90f7a3019edfbf850bcb4
Carlos <carlos.bergamasco@gmail.com>
2020-08-10 15:25:18 -0300

    Adjust configurations

--- src/main/java/com/berga/orderproducts/util/HibernateUtil.java
+++ src/main/java/com/berga/orderproducts/util/HibernateUtil.java
@@ -6,13 +6,20 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import com.berga.orderproducts.model.Category;
+import com.berga.orderproducts.model.Order;
+import com.berga.orderproducts.model.Product;
+
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
 
 	static {
 		// loads configurations and mappings
-		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		Configuration configuration = new Configuration().configure();
+		configuration.addAnnotatedClass(Category.class);
+		configuration.addAnnotatedClass(Product.class);
+		configuration.addAnnotatedClass(Order.class);
 		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
 				.applySettings(configuration.getProperties()).build();
 
