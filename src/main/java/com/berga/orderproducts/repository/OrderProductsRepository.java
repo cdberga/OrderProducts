807e3df86444beeee9867653a48b5f729193e4b1
Carlos <carlos.bergamasco@gmail.com>
2020-08-10 18:04:37 -0300

    Add new queries

--- src/main/java/com/berga/orderproducts/repository/OrderProductsRepository.java
+++ src/main/java/com/berga/orderproducts/repository/OrderProductsRepository.java
@@ -7,6 +7,7 @@ import javax.persistence.Query;
 import org.hibernate.Session;
 
 import com.berga.orderproducts.model.Category;
+import com.berga.orderproducts.model.Product;
 import com.berga.orderproducts.util.HibernateUtil;
 
 public class OrderProductsRepository {
@@ -26,6 +27,45 @@ public class OrderProductsRepository {
 	public void insertCategory(Integer id, String name) {
 		Session session = HibernateUtil.getSession();
 		session.save(new Category(name));
+		HibernateUtil.closeSession(session);
+	}
+	
+	public void findProductsByCategoryName(String name) {
+		Session session = HibernateUtil.getSession();
+		String hql = "from Product where Category.name = :category";
+		Query query = session.createQuery(hql);
+		query.setParameter("category", name);
+
+		List<Product> products = query.getResultList();
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+		HibernateUtil.closeSession(session);
+	}
+	
+	public void findProductsByDescription(String desc) {
+		Session session = HibernateUtil.getSession();
+		String hql = "from Product where description like :description";
+		Query query = session.createQuery(hql);
+		query.setParameter("description", "%" + desc + "%");
+		
+		List<Product> products = query.getResultList();
+		for (Product product : products) {
+			System.out.println(product.getName());
+		}
+		HibernateUtil.closeSession(session);
+	}
+	
+	public void updateProduct(Product p) {
+		Session session = HibernateUtil.getSession();
+		String hql = "update Product set price = :price where id=:id";
+		Query query = session.createQuery(hql);
+		query.setParameter("price", p.getPrice());
+		query.setParameter("id", p.getId());
+		int rowsAffected = query.executeUpdate();
+		if(rowsAffected > 0 ) {
+			System.out.println(rowsAffected + " Products updated.");
+		}
 		HibernateUtil.closeSession(session);
 	}
 }
